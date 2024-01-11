ef6167c9911ee28bb9f22a5cd8f1ffd710e7e630
lakami <ossowska.kamila1@gmail.com>
2024-01-11 12:21:06 +0100

    commit AddClientToTrip (POST)

--- zad7_apbd/Controllers/TripsController.cs
+++ zad7_apbd/Controllers/TripsController.cs
@@ -42,4 +42,64 @@ public class TripsController : ControllerBase
             })
             .ToListAsync();
     }
+    
+    [HttpPost("{idTrip}/clients")]
+    public async Task<IActionResult> AddClientToTrip(int idTrip, AddClientToTripDTO addClientToTripDTO)
+    {
+        var trip = await _context.Trips
+            //dadanie Include, aby zrobiło JOIN z tabelą `ClientTrips`
+            .Include(t => t.ClientTrips)
+            .FirstOrDefaultAsync(t => t.IdTrip == idTrip);
+        
+        var client = await _context.Clients
+            .FirstOrDefaultAsync(c => c.Pesel == addClientToTripDTO.Pesel);
+        
+        //sprawdzenie czy podana wycieczka istnieje
+        if (trip == null)
+        {
+            return NotFound("Nie znaleziono wycieczki");
+        }
+        
+        // sprawdzenie czy są jeszcze wolne miejsca na tą wycieczkę
+        // jeśli są wolne miejsca mogę dodać nowego klienta lub przypisać istniejącego klienta
+         if (trip.ClientTrips.Count >= trip.MaxPeople)
+         {
+             return BadRequest("Wszytkie miejsca na podaną wycieczkę są już zajęte");
+         }
+        
+        //sprawszenie czy klient o podanym numerze PESEL istnieje, jeśli nie dodanie do bazy danych
+        if (client == null)
+        {
+            client = new Client
+            {
+                FirstName = addClientToTripDTO.FirstName,
+                LastName = addClientToTripDTO.LastName,
+                Email = addClientToTripDTO.Email,
+                Telephone = addClientToTripDTO.Telephone,
+                Pesel = addClientToTripDTO.Pesel
+            };
+            await _context.Clients.AddAsync(client);
+            await _context.SaveChangesAsync();
+            _logger.LogInformation("Dodano nowego klienta do bazy danych");
+        }
+        
+        //sprawdzenie czy klient jest już zapisany na daną wycieczkę
+        if (trip.ClientTrips.Any(ct => ct.IdClient == client.IdClient))
+        {
+            return BadRequest("Klient jest już zapisany na tę wycieczkę");
+        }
+
+        var clientTrip = new ClientTrip
+        {
+            IdClient = client.IdClient,
+            IdTrip = trip.IdTrip,
+            RegisteredAt = DateTime.Now,
+            PaymentDate = addClientToTripDTO.PaymentDate
+        };
+        
+        await _context.ClientTrips.AddAsync(clientTrip);
+        await _context.SaveChangesAsync();
+        return Ok("Dodano klienta do wycieczki");
+    }
+    
 }
---
+++ zad7_apbd/Models/AddClientToTrip.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace zad7_apbd.Models;
+
+public class AddClientToTripDTO
+{
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    public string Telephone { get; set; }
+    [Required]
+    public string Pesel { get; set; }
+    [Required]
+    public int IdTrip { get; set; }
+    [Required]
+    public string TripName { get; set; }
+    public DateTime? PaymentDate { get; set; }
+}
+
+
